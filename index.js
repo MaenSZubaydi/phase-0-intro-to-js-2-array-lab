6eb9155bdf7f17e5ff09e796ef94fe8b27a68e5d
MaenSZubaydi <maenzubaydi@gmail.com>
2022-05-08 22:29:38 +1000

    new

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(element){
+    cats.push(element);
+}
+
+destructivelyAppendCat('Ralph');
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyPrependCat(element){
+    cats.unshift(element);
+}
+
+destructivelyPrependCat('Bob');
+
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+destructivelyRemoveLastCat();
+
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+destructivelyRemoveFirstCat();
+
+cats.length = 0;
+cats.push('Milo', 'Otis', 'Garfield');
+
+
+function appendCat(element){
+    return [...cats, element];
+}
+
+const copyCats1 = appendCat("Broom");
+
+function prependCat(element){
+    return [element, ...cats];
+}
+
+const copyCats2 = prependCat("Arnold");
+
+function removeLastCat(){
+    return cats.slice(0,-1);
+}
+
+const copyCats3 = removeLastCat();
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
+removeFirstCat();
+
+
